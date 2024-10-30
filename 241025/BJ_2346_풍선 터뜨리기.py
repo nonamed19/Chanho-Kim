e9474e5808174d1ddd938754c4600eb5c0b188a9
Chanho Kim <nonamed19@naver.com>
2024-10-30 15:51:06 +0900

    241030

--- 241025/BJ_2346_풍선 터뜨리기.py
+++ 241025/BJ_2346_풍선 터뜨리기.py
@@ -1,22 +1,23 @@
 N = int(input())
-orders = list(range(1, N + 1))
-numbers = list(map(int, input().split()))
+orders = list(range(1, N + 1))  # 초기 순서 저장
+numbers = list(map(int, input().split()))  # 이동해야 하는 칸의 수
 
 arrays = [[0, 0] for _ in range(N)]
 for i in range(N):
-    arrays[i] = [numbers[i], orders[i]]
+    arrays[i] = [numbers[i], orders[i]]  # [이동해야 하는 칸의 수, 초기 순서]
 
 results = []
 idx = 0  # initialization
 while arrays:
     move = arrays[idx][0]
-    results.append(arrays[idx][1])
-    arrays.pop(idx)
+    results.append(arrays[idx][1])  # move의 순서에 따라 results에 초기 순서 저장
+    arrays.pop(idx)  # move의 순서에 따라 arrays에서 element 추출
 
     # 종료 조건(마지막 element에 대한 계산이 완료된 경우)
     if not arrays:
         break
 
+    # python index 로직에 따라 move가 양수인 경우와 음수인 경우를 나누어 계산해야 함.
     if move > 0:
         idx = (idx + move - 1) % len(arrays)
     else:  # elif move <= 0:
--- 241025/BJ_4949_균형잡힌 세상.py
+++ 241025/BJ_4949_균형잡힌 세상.py
@@ -1,26 +1,28 @@
 while True:
     words = list(input())
-    # 종료 조건
+    # 종료 조건(온점 하나만 input으로 들어오는 경우)
     if len(words) == 1:
         break
 
     queue = []
     for word in words:
+        # character가 소괄호 / 대괄호인 경우 queue에 삽입
         if word == '(' or word == ')' or word == '[' or word == ']':
             queue.append(word)
 
+        # 소괄호 / 대괄호가 queue에 삽입된 후 가장 최근에 삽입된 character와 짝이 맞는지 확인
         if len(queue) >= 2:
             word2 = queue.pop()
             word1 = queue.pop()
-            if word1 == '(' and word2 == ')':
+            if word1 == '(' and word2 == ')':  # 소괄호인 경우 queue에서 pop()
                 pass
-            elif word1 == '[' and word2 == ']':
+            elif word1 == '[' and word2 == ']':  # 대괄호인 경우 queue에서 pop()
                 pass
-            else:
+            else:  # 소괄호 & 대괄호인 경우 queue에 다시 삽입
                 queue.append(word1)
                 queue.append(word2)
 
-    if queue:
+    if queue:  # queue에 element가 남아있는 경우 짝이 맞지 않다는 것을 의미
         print('no')
-    else:
+    else:  # queue에 element가 남아있지 않는 경우 모든 짝이 맞다는 것을 의미
         print('yes')
